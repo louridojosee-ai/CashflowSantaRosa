792971e307087d7ab8c3925c7dbe7ce6a211b7cb
louridojosee-ai <lourido.josee@gmail.com>
2026-04-28 16:24:21 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CashFlowObra from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CashFlowObra />
-  </React.StrictMode>
-)
